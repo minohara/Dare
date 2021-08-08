79e9f87a5661fd8bf683cb5235daa196f017c900
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2021-08-08 16:06:36 +0900

    Read the light sensor

--- app/src/main/java/org/minohara/dare/MainActivity.java
+++ app/src/main/java/org/minohara/dare/MainActivity.java
@@ -2,13 +2,47 @@ package org.minohara.dare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor mLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float lux = event.values[0];
+        ((TextView)findViewById(R.id.light)).setText(String.format("%8.2f",lux));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
