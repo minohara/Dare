be0b0044c29636a12c360b780c67cc22abb37b11
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2021-11-11 18:19:36 +0900

    Bluetooth LE Data Communication (Send data to Server)

--- app/src/main/java/org/minohara/dare/DataServer.java
+++ app/src/main/java/org/minohara/dare/DataServer.java
@@ -18,9 +18,11 @@ import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDeviceConnection;
 import android.os.Build;
 import android.os.ParcelUuid;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -42,34 +44,27 @@ public class DataServer {
     static final UUID MESSAGE_UUID = UUID.fromString("f03fde31-928a-4c87-afff-6817e6ffe43a");
     static final UUID CONFIRM_UUID = UUID.fromString("855d6a12-478f-48f5-9171-b9e25800cb42");
     static int grantedRequest = 0;
-
     private Context app = null;
+    private Activity activity = null;
     private BluetoothManager bluetoothManager = null;
     private BluetoothAdapter adapter = null;
-
     private BluetoothLeAdvertiser advertiser = null;
     private AdvertiseCallback advertiseCallback = null;
     private AdvertiseSettings advertiseSettings = null;
     private AdvertiseData advertiseData = null;
-
     private MutableLiveData<String> _message;
     private LiveData<String> message = _message;
-
     private MutableLiveData<BluetoothDevice> _connectionRequest;
     private LiveData<BluetoothDevice> connectionRequest = _connectionRequest;
-
     private MutableLiveData<Boolean> _requestEnableBluetooth;
     private LiveData<Boolean> requestEnableBluetooth = _requestEnableBluetooth;
-
     private BluetoothGattServer gattServer;
     private BluetoothGattServerCallback gattServerCallback = null;
-
     private BluetoothGatt gattClient;
     private BluetoothGattCallback gattClientCallback = null;
-
     private BluetoothDevice currentDevice;
     //private MutableLiveData<DeviceConnectionState> _deviceConnection;
-    //private LiveData<DeviceConnectionState> deviceConnection = _deviceConnection;
+    //private LiveData<DeviceConnectionState> deviceConnection = _deviceConnection
     private BluetoothGatt gatt;
     private BluetoothGattCharacteristic messageCharacteristic;
 
@@ -77,69 +72,22 @@ public class DataServer {
     void startServer(Context context, Activity activity) {
         Log.d(TAG, "Server Start");
         app = context;
-
+        this.activity = activity;
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
             return;
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_ADVERTISE)
-                == PackageManager.PERMISSION_GRANTED) {
-           grantedRequest |= REQUEST_BLUETOOTH_ADVERTISE;
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT)
-                    == PackageManager.PERMISSION_GRANTED) {
-                grantedRequest |= REQUEST_BLUETOOTH_CONNECT;
-                if (ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    grantedRequest |= REQUEST_BLUETOOTH_SCAN;
-                    if (bluetoothManager == null) {
-                        bluetoothManager
-                                = (BluetoothManager) app.getSystemService(Context.BLUETOOTH_SERVICE);
-                        adapter = bluetoothManager.getAdapter();
-                    }
-                    if (_requestEnableBluetooth == null) {
-                        _requestEnableBluetooth = new MutableLiveData<Boolean>();
-                    }
-                    if (!adapter.isEnabled()) {
-                        _requestEnableBluetooth.setValue(true);
-                    } else {
-                        _requestEnableBluetooth.setValue(false);
-                        if (adapter.isMultipleAdvertisementSupported()) {
-                            setupGattServer(app);
-                            startAdvertisement();
-                        }
-                    }
-                } else {
-                    activity.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_SCAN},
-                            REQUEST_BLUETOOTH_SCAN);
-                }
-            } else {
-                activity.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT},
-                        REQUEST_BLUETOOTH_CONNECT);
+            if (bluetoothManager == null) {
+                bluetoothManager = (BluetoothManager) app.getSystemService(Context.BLUETOOTH_SERVICE);
+                adapter = bluetoothManager.getAdapter();
             }
-        } else {
-            activity.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_ADVERTISE},
-                    REQUEST_BLUETOOTH_ADVERTISE);
-        }
+            if (adapter.isMultipleAdvertisementSupported()) {
+                Log.d(TAG, "Server supported");
+                setupGattServer(app);
+                startAdvertisement();
+            }
     }
 
     void stopServer() {
         stopAdevertisement();
-    }
-
-    String getyourDeviceAddress() {
-        return bluetoothManager.getAdapter().getAddress();
-    }
-
-    void setCurrentConnection(BluetoothDevice device) {
-        currentDevice = device;
-        /*
-        if (_deviceConnection == null) {
-            _deviceConnection = new MutableLiveData<DeviceConnectionState>();
-        }*/
-        connectToDevice(device);
-    }
-
-    private void connectToDevice(BluetoothDevice device) {
-        gattClientCallback = new GattClientCallback();
-        gattClient = device.connectGatt(app,false, gattClientCallback);
     }
 
     Boolean sendMessage(String message) {
@@ -151,47 +99,79 @@ public class DataServer {
             boolean success = gatt.writeCharacteristic(messageCharacteristic);
             if (success) {
                 Log.d(TAG, "onServiceDiscovered: message send: true");
-                _message.setValue(message);
+                //_message.setValue(message);
             } else {
                 Log.d(TAG, "onServiceDiscovered: message send: false");
             }
-        }
-        else {
+        } else {
             Log.d(TAG, "sendMessage: no gatt connection to send a message with");
         }
         return false;
     }
-
+//----------------------------------------------------------server----------------------------------------------------------
     private void setupGattServer(Context app) {
         Log.d(TAG, "Set up GATT server");
         gattServerCallback = new GattServerCallback();
-
         gattServer = bluetoothManager.openGattServer(app, gattServerCallback);
         gattServer.addService(setupGattService());
     }
 
     private BluetoothGattService setupGattService() {
         Log.d(TAG, "Set up GATT service");
-        BluetoothGattService service
-                = new BluetoothGattService(SERVICE_UUID,BluetoothGattService.SERVICE_TYPE_PRIMARY);
-        BluetoothGattCharacteristic messageCharacteristic = new BluetoothGattCharacteristic(
-                MESSAGE_UUID,
-                BluetoothGattCharacteristic.PROPERTY_WRITE,
-                BluetoothGattCharacteristic.PERMISSION_WRITE
-        );
+        BluetoothGattService service = new BluetoothGattService(SERVICE_UUID,BluetoothGattService.SERVICE_TYPE_PRIMARY);
+        BluetoothGattCharacteristic messageCharacteristic = new BluetoothGattCharacteristic(MESSAGE_UUID, BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_WRITE);
         service.addCharacteristic(messageCharacteristic);
-        BluetoothGattCharacteristic confirmCharacteristic = new BluetoothGattCharacteristic(
-                CONFIRM_UUID,
-                BluetoothGattCharacteristic.PROPERTY_WRITE,
-                BluetoothGattCharacteristic.PERMISSION_WRITE
-        );
+        BluetoothGattCharacteristic confirmCharacteristic = new BluetoothGattCharacteristic(CONFIRM_UUID, BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_WRITE);
         service.addCharacteristic(confirmCharacteristic);
-
         return service;
     }
 
-    private void startAdvertisement() {
+    private class GattServerCallback extends BluetoothGattServerCallback {
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            super.onConnectionStateChange(device, status, newState);
+            boolean isSuccess = (status == BluetoothGatt.GATT_SUCCESS);
+            boolean isConnected = (newState == BluetoothProfile.STATE_CONNECTED);
+            Log.d(TAG, String.format("onConnectionStateChange: Server %s %s" + "succress: %s connected: %s", device.toString(), device.getName(), isSuccess?"true":"false", isConnected?"true":"false"));
+            if (isSuccess && isConnected) {
+                _connectionRequest.postValue(device);
+            } else {
+                //_deviceConnection.postValue(deviceConnectionState.Disconnected);
+            }
+        }
 
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+            super.onCharacteristicWriteRequest(device, requestId, characteristic, preparedWrite, responseNeeded, offset, value);
+            if (characteristic.getUuid() == MESSAGE_UUID) {
+                if (gattServer != null) {
+                    gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, null);
+                    if (value != null) {
+                        String message = new String(value, StandardCharsets.UTF_8);
+                        Log.d(TAG, String.format("onCharacteristicWriteRequest: Have message: %s", message));
+
+                        if (message != null) {
+                            ((TextView)activity.findViewById(R.id.message))
+                                    .setText(message);
+                            //_message.postValue(RemoteMessage(message));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    AdvertiseData buildAdvertiseData() {
+        ParcelUuid parcelUuid = new ParcelUuid((SERVICE_UUID));
+        AdvertiseData.Builder dataBuilder = new AdvertiseData.Builder().addServiceUuid(parcelUuid).setIncludeDeviceName(true);
+        return dataBuilder.build();
+    }
+
+    AdvertiseSettings buildAdevertiseSettings() {
+        return new AdvertiseSettings.Builder().setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER).setTimeout(0).build();
+    }
+
+    private void startAdvertisement() {
         advertiser = adapter.getBluetoothLeAdvertiser();
         advertiseSettings = buildAdevertiseSettings();
         advertiseData = buildAdvertiseData();
@@ -199,11 +179,9 @@ public class DataServer {
             Log.d(TAG, String.format("startAdvertisement: with advertiser %s", advertiser.toString()));
             if (advertiseCallback == null) {
                 advertiseCallback = new DeviceAdvertiseCallback();
-
                 advertiser.startAdvertising(advertiseSettings, advertiseData, advertiseCallback);
             }
-        }
-        else {
+        } else {
             Log.d(TAG, "can't get advertiser");
         }
     }
@@ -214,95 +192,6 @@ public class DataServer {
             Log.d(TAG, String.format("stopAdvertisement: with advertiser %s", advertiser.toString()));
             advertiser.stopAdvertising(advertiseCallback);
             advertiseCallback = null;
-        }
-    }
-
-    AdvertiseData buildAdvertiseData() {
-        ParcelUuid parcelUuid = new ParcelUuid((SERVICE_UUID));
-        AdvertiseData.Builder dataBuilder = new AdvertiseData.Builder()
-                .addServiceUuid(parcelUuid)
-                .setIncludeDeviceName(true);
-        return dataBuilder.build();
-    }
-
-    AdvertiseSettings buildAdevertiseSettings() {
-        return new AdvertiseSettings.Builder()
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
-                .setTimeout(0)
-                .build();
-    }
-
-    private class GattServerCallback extends BluetoothGattServerCallback {
-        @Override
-        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
-            super.onConnectionStateChange(device, status, newState);
-            boolean isSuccess = (status == BluetoothGatt.GATT_SUCCESS);
-            boolean isConnected = (newState == BluetoothProfile.STATE_CONNECTED);
-            Log.d(TAG,
-                    String.format("onConnectionStateChange: Server %s %s"
-                            + "succress: %s connected: %s",
-                            device.toString(), device.getName(),
-                            isSuccess?"true":"false", isConnected?"true":"false")
-            );
-            if (isSuccess && isConnected) {
-                _connectionRequest.postValue(device);
-            }
-            else {
-                //_deviceConnection.postValue(DeviceConnectionState.Disconnected);
-            }
-        }
-
-        @Override
-        public void onCharacteristicWriteRequest(
-                BluetoothDevice device,
-                int requestId,
-                BluetoothGattCharacteristic characteristic,
-                boolean preparedWrite,
-                boolean responseNeeded,
-                int offset,
-                byte[] value) {
-            super.onCharacteristicWriteRequest(device, requestId, characteristic,
-                    preparedWrite, responseNeeded, offset, value);
-            if (characteristic.getUuid() == MESSAGE_UUID) {
-                if (gattServer != null) {
-                    gattServer.sendResponse(device, requestId,
-                            BluetoothGatt.GATT_SUCCESS, 0, null);
-                    if (value != null) {
-                        String message = new String(value, StandardCharsets.UTF_8);
-                        Log.d(TAG, String.format("onCharacteristicWriteRequest: Have message: %s",
-                               message));
-                        if (message != null) {
-                            //_message.postValue(RemoteMessage(message));
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private class GattClientCallback extends BluetoothGattCallback {
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            super.onConnectionStateChange(gatt, status, newState);
-            boolean isSuccess = (status == BluetoothGatt.GATT_SUCCESS);
-            boolean isConnected = (newState == BluetoothProfile.STATE_CONNECTED);
-            Log.d(TAG, String.format("onConnectionStateChange: Client %s success: %s connected: %s",
-                    gatt.toString(), isSuccess?"true":"false", isConnected?"true":"false"));
-            if (isSuccess && isConnected) {
-                gatt.discoverServices();
-            }
-        }
-
-        @Override
-        public void onServicesDiscovered(BluetoothGatt discoveredGatt, int status) {
-            super.onServicesDiscovered(discoveredGatt, status);
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-                Log.d(TAG, String.format("onServiceDiscovered: Have gatt %s",
-                        discoveredGatt.toString()));
-                gatt = discoveredGatt;
-                BluetoothGattService service = discoveredGatt.getService(SERVICE_UUID);
-                messageCharacteristic = service.getCharacteristic(MESSAGE_UUID);
-            }
         }
     }
 
@@ -319,4 +208,38 @@ public class DataServer {
             Log.d(TAG, "Advertiseing successfully started");
         }
     }
+//----------------------------------------------------------server----------------------------------------------------------
+    void setCurrentConnection(BluetoothDevice device) {
+        currentDevice = device;
+        connectToDevice(device);
+    }
+
+    private void connectToDevice(BluetoothDevice device) {
+        gattClientCallback = new GattClientCallback();
+        gattClient = device.connectGatt(app,false, gattClientCallback);
+    }
+//----------------------------------------------------------client----------------------------------------------------------
+    private class GattClientCallback extends BluetoothGattCallback {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            boolean isSuccess = (status == BluetoothGatt.GATT_SUCCESS);
+            boolean isConnected = (newState == BluetoothProfile.STATE_CONNECTED);
+            Log.d(TAG, String.format("onConnectionStateChange: Client %s success: %s connected: %s", gatt.toString(), isSuccess?"true":"false", isConnected?"true":"false"));
+            if (isSuccess && isConnected) {
+                gatt.discoverServices(); // サービス取得要求
+            }
+        }
+        @Override
+        public void onServicesDiscovered(BluetoothGatt discoveredGatt, int status) {
+            super.onServicesDiscovered(discoveredGatt, status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(TAG, String.format("onServiceDiscovered: Have gatt %s", discoveredGatt.toString()));
+                gatt = discoveredGatt;
+                BluetoothGattService service = discoveredGatt.getService(SERVICE_UUID);
+                messageCharacteristic = service.getCharacteristic(MESSAGE_UUID);
+            }
+        }
+    }
+//----------------------------------------------------------client----------------------------------------------------------
 }
--- app/src/main/java/org/minohara/dare/MainActivity.java
+++ app/src/main/java/org/minohara/dare/MainActivity.java
@@ -2,17 +2,21 @@ package org.minohara.dare;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -28,40 +32,38 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Dare";
     private DataServer dataServer;
     private SensorData sensorData;
-
     private TextView textView;
-
     private BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
     private BluetoothLeScanner scanner = null;
     private List<ScanFilter> scanFilters;
+    private static final int REQUEST_ENABLE_BT = 1000;
+    private static final long SCAN_PERIOD = 10000;
+    private ArrayList<BluetoothDevice> deviceList = new ArrayList<>();
     ScanSettings scanSettings;
 
-    private void startScan() {
-        if (scanner == null) {
-            scanner = adapter.getBluetoothLeScanner();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.message);
+        dataServer = new DataServer();
+        sensorData = new SensorData(getApplicationContext());
+        scanFilters = buildScanFilters();
+        scanSettings = buildScanSettings();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_ADVERTISE, Manifest.permission.BLUETOOTH_SCAN}, 1001);
+        } else {
+            dataServer.startServer(getApplicationContext(), this);
         }
-        textView.setText("");
-        scanner.startScan(scanFilters, scanSettings, scanCallback);
-        //scanner.startScan(scanCallback);
-        Log.d(TAG, "Scan started");
     }
-
-    private ScanCallback scanCallback =
-            new ScanCallback() {
-                @Override
-                public void onScanResult(int type, ScanResult result) {
-                    super.onScanResult(type, result);
-                    textView.append(result.toString());
-                    Log.d(TAG, String.format("onScanResult: %s", result.toString()));
-                }
-            };
-
-    private List<ScanFilter> buildScanFiters() {
+//----------------------------------------------------------Scan----------------------------------------------------------
+    private List<ScanFilter> buildScanFilters() {
         ScanFilter.Builder builder = new ScanFilter.Builder();
         builder.setServiceUuid(new ParcelUuid(DataServer.SERVICE_UUID));
         List<ScanFilter> filters = new ArrayList<ScanFilter>();
         filters.add(builder.build());
-        Log.d(TAG, filters.toString());
         return filters;
     }
 
@@ -71,78 +73,80 @@ public class MainActivity extends AppCompatActivity {
         return builder.build();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        textView = (TextView)findViewById(R.id.message);
-        dataServer = new DataServer();
-        sensorData = new SensorData(getApplicationContext());
-
-        scanFilters = buildScanFiters();
-        scanSettings = buildScanSettings();
+    private void startScan() {
+        if (scanner == null) {
+            scanner = adapter.getBluetoothLeScanner();
+        }
+        textView.setText("");
+        scanner.startScan(scanFilters, scanSettings, scanCallback);
+        Log.d(TAG, "Scan started");
     }
 
-    // 画面にメッセ-ジを出すための処理
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int type, ScanResult result) {
+            super.onScanResult(type, result);
+            saveDevice(result.getDevice());
+            textView.append(result.toString());
+            Log.d(TAG, String.format("onScanResult: %s", result.toString()));
+            if (deviceList != null) {
+                Log.d(TAG, String.format("complete save deviceList"));
+            }
+        }
+    };
+
+    public void saveDevice(BluetoothDevice device) {
+        if (deviceList == null) {
+            deviceList = new ArrayList<>();
+        }
+        deviceList.add(device);
+    }
+//----------------------------------------------------------Scan----------------------------------------------------------
     public void writeMsg(String text) {
         ((TextView)findViewById(R.id.message)).setText(text);
     }
 
-    // Searchボタンが押されたときの処理
-    public void searchBtn(View view) {
+    public void search(View view) {
         startScan();
+    }
+
+    public void Connect(View vie) {
+        dataServer.setCurrentConnection(deviceList.get(0));
+    }
+
+    public void Close(View vi) { dataServer.stopServer(); }
+
+    public void Send(View v){
+        String aaa = "1234";
+        dataServer.sendMessage(aaa);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         sensorData.startSensor();
-        if (DataServer.grantedRequest == (DataServer.REQUEST_BLUETOOTH_ADVERTISE
-                | DataServer.REQUEST_BLUETOOTH_CONNECT | DataServer.REQUEST_BLUETOOTH_SCAN)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                dataServer.startServer(getApplicationContext(), this);
-        }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         sensorData.stopSensor();
-        dataServer.stopServer();
+        //dataServer.stopServer();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResult) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResult);
-        switch (requestCode) {
-            case DataServer.REQUEST_BLUETOOTH_ADVERTISE:
-                if (grantResult.length < 1 || grantResult[0] != PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "BLUTOOTH_ADVERTISE is not permitted");
-                } else {
-                    DataServer.grantedRequest |= DataServer.REQUEST_BLUETOOTH_ADVERTISE;
+        if (requestCode == 1001 ) {
+            for (int i = 0; i < grantResult.length; i++) {
+                if (grantResult[i] != PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, String.format("%s is not permitted", permissions[i].toString()));
                 }
-                break;
-            case DataServer.REQUEST_BLUETOOTH_CONNECT:
-                if (grantResult.length < 1 || grantResult[0] != PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "BLUTOOTH_CONNECT is not permitted");
-                } else {
-                    DataServer.grantedRequest|= DataServer.REQUEST_BLUETOOTH_CONNECT;
-                }
-                break;
-            case DataServer.REQUEST_BLUETOOTH_SCAN:
-                if (grantResult.length < 1 || grantResult[0] != PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "BLUTOOTH_SCAN is not permitted");
-                } else {
-                    DataServer.grantedRequest |= DataServer.REQUEST_BLUETOOTH_SCAN;
-                }
-                break;
-        }
-        Log.d(TAG, String.format("grantedRequest:%d", DataServer.grantedRequest));
-        if (DataServer.grantedRequest == (DataServer.REQUEST_BLUETOOTH_ADVERTISE
-                | DataServer.REQUEST_BLUETOOTH_CONNECT | DataServer.REQUEST_BLUETOOTH_SCAN)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 dataServer.startServer(getApplicationContext(), this);
+            }
         }
     }
 }
